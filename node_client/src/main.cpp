e86b092a4df286b56e66da1d4d84424727889064
Ziemin <brentprey@gmail.com>
2013-09-06 20:09:24 +0200

    Reorganized cmake project

---
+++ node_client/src/main.cpp
@@ -0,0 +1,11 @@
+#include "../../sercli/src/sp2p.hpp"
+
+/**
+ * Simple program to communicate with node and manage 
+ * sp2p related data.
+ * It primarily makes use of sp2p sercli library.
+ */
+
+int main() {
+    return 0;
+}
