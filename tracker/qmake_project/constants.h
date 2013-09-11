a33b687775222c735f46f3abf336a1eb15785059
Jakub Czarnowicz <j.czarnowicz@gmail.com>
2013-09-12 01:01:28 +0200

    Added cert signing by CA, corrected db_init.sql, added example cert and priv_key for tracker

--- tracker/qmake_project/constants.h
+++ tracker/qmake_project/constants.h
@@ -7,8 +7,12 @@ namespace sp2p {
 namespace tracker {
 namespace consts {
 
+const std::string CERT_PATH = "server.pem";
+const std::string PRIV_KEY_PATH = "server.prv";
+
 const int USER_TTL = 1000*60*30; // 30 min
 const int SERVER_TTL = 1000*60*30; // 30 min
+const std::uint64_t CERT_TTL = 1000*60*60*24*365L;
 
 const std::string SERVER_ADDRESS = "127.0.0.1";
 const std::string SERVER_PORT = "1234";
--- tracker/qmake_project/utils/util_functions.h
+++ tracker/qmake_project/utils/util_functions.h
@@ -1,11 +1,19 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef UTILSSP2P_H
+#define UTILSSP2P_H
 
 #include <string>
 #include <pqxx/result>
 #include <boost/asio/streambuf.hpp>
 #include <botan/botan.h>
 #include <botan/bcrypt.h>
+#include <botan/init.h>
+#include <botan/auto_rng.h>
+#include <botan/x509self.h>
+#include <botan/rsa.h>
+#include <botan/dsa.h>
+#include <botan/data_src.h>
+#include <botan/pkcs10.h>
+#include <botan/x509_ca.h>
 
 namespace sp2p{
 namespace tracker{
@@ -19,6 +27,11 @@ bool toBool(pqxx::tuple::reference);
 int toInt(pqxx::tuple::reference);
 std::string getPasswordHash(std::string &password);
 
+void init_ca(const std::string &certPath, const std::string &privateKeyPath);
+
+std::string signCertyficate(std::string &reques);
+
+std::string &getNodeCert();
 
 template <std::size_t N>
 std::int64_t getNumFromBuff(boost::array<char, N> &buff, int numSize)
